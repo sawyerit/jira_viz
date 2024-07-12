f60b4d386970fce3aa13ff737bc42773cc866b0c
Shane Sawyer <shane.sawyer@gmail.com>
2024-07-12 07:56:13 -0600

    refactor

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ color_dict = {
 }
 
 # Connect to Jira using the Jira Python API
-jira_instance = jira.JIRA(server="https://dhi-jira.atlassian.net", basic_auth=("username", "key"))
+jira_instance = jira.JIRA(server="https://<subdomain>.atlassian.net", basic_auth=("username", "key"))
 
 
 def set_color(issue):
